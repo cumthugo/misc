f7914baff77b6b22397773b6aceaf35760c96b7d
ZhangYong <yzhang66@yfve.com.cn>
2016-05-03 17:03:40 +0800

    add 2 sample result test

--- ReadCalData.py
+++ ReadCalData.py
@@ -35,6 +35,18 @@ def get_WriteCal(text):
     write_cal_text = get_text(text,'Starting Write calibration','Byte 0')
     return get_Cal(write_cal_text)
 
+
+def hc_abs_value(hc,abs):
+    hc_v = int(hc,16)
+    abs_v = int(abs,16)
+    return (hc_v << 8) + abs_v
+
+def hc_string(v):
+    return "0x%02X"%(v>>8)
+
+def abs_string(v):
+    return "0x%02X"%(v&0xFF)
+
 def get_Byte(text):
     ret = []
     byte_text = [get_text(text,'BYTE '+str(i),'BYTE') for i in range(3)]
@@ -42,11 +54,11 @@ def get_Byte(text):
     for i in range(4):
         start_hc, start_abs = text_find('Start:\s*HC=(0x[0-9A-F]*) ABS=(0x[0-9A-F]*)',byte_text[i])       
         end_hc, end_abs = text_find('End:\s*HC=(0x[0-9A-F]*) ABS=(0x[0-9A-F]*)',byte_text[i])        
-        ret.append({'start':[start_hc,start_abs],'end':[end_hc,end_abs]})
+        ret.append([hc_abs_value(start_hc,start_abs),hc_abs_value(end_hc,end_abs)])
     return ret
 
-def get_MMDC_MPWLDECTRL(i,text):
-    return text_find('MMDC_MPWLDECTRL'+str(i)+'.*?= (0x[0-9A-F]*)',text)
+def get_MMDC_MPWLDECTRL(text):
+    return text_find('MMDC_MPWLDECTRL0.*?= (0x[0-9A-F]*)',text),text_find('MMDC_MPWLDECTRL1.*?= (0x[0-9A-F]*)',text)
 
 def get_file_context(file_path):
      f = open(file_path)
@@ -55,8 +67,7 @@ def get_file_context(file_path):
 def check_file(file_path):
     try:
         t = get_file_context(file_path)
-        ctrl0 = get_MMDC_MPWLDECTRL(0,t)
-        ctrl1 = get_MMDC_MPWLDECTRL(1,t)
+        ctrl = get_MMDC_MPWLDECTRL(t)
         byte = get_Byte(t)
         read_cat = get_ReadCal(t)
         write_cat = get_WriteCal(t)
@@ -64,32 +75,39 @@ def check_file(file_path):
     except:
         return False
 
-def hc_abs_value(hc,abs):
-    hc_v = int(hc,16)
-    abs_v = int(abs,16)
-    return (hc_v << 8) + abs_v
-
-def calc_hc_abs(byte):
-    start = byte['start']
-    end = byte['end']
-    start_hc_v = int(start[0],16)
-    start_abs_v = int(start[1],16)
-    end_hc_v = int(end[0],16)
-    end_abs_v = int(end[1],16)
-    mean = ((start_hc_v+end_hc_v)/2) << 8
-    mean += (start_abs_v+end_abs_v)/2 
-    end_half = ((end_hc_v-1) << 8) + end_abs_v
+def calc_hc_abs_final(byte):
+    mean = (byte[0] + byte[1]) / 2
+    end_half = byte[1] - (1<<8)
     final = max(mean,end_half)
+    print 'Mean: HC=',hc_string(mean),' ABS=',abs_string(mean)
+    print 'end_half: HC=',hc_string(end_half),' ABS=',abs_string(end_half)
+    print 'final: HC=',hc_string(final),' ABS=',abs_string(final)
     return final
 
 def calc_DQS_cal(byte):
     b = []
     for i in range(4):
-        b.append(calc_hc_abs(byte[i]))
+        #print 'Byte',i,'\nStart: HC=',hc_string(byte[i][0]),' ABS=',abs_string(byte[i][0])
+        #print 'End: HC=',hc_string(byte[i][1]),' ABS=',abs_string(byte[i][1])
+        b.append(calc_hc_abs_final(byte[i]))
     MPDG_ctrl0 = (b[1] << 16) + b[0]
     MPDG_ctrl1 = (b[3] << 16) + b[2]
     return "0x%08X"%MPDG_ctrl0, "0x%08X"%MPDG_ctrl1
 
+def calc_DQS_cals(bytes):
+    new_byte = []
+    #find max start and min end for each byte
+    for i in range(4):
+        start_arr = []; end_arr = []
+        for byte in bytes:
+            start_arr.append(byte[i][0])
+            end_arr.append(byte[i][1])
+        start_v = max(start_arr)
+        end_v = min(end_arr)
+        #print 'Byte',i,'\nStart: HC=',hc_string(start_v),' ABS=',abs_string(start_v)
+        #print 'End: HC=',hc_string(end_v),' ABS=',abs_string(end_v)
+        new_byte.append([start_v,end_v])
+    return calc_DQS_cal(new_byte)
  
 def calc_cal_start_end(cal_arr,index):
     s=0;e=0
@@ -111,6 +129,26 @@ def calc_cal_value(cal_arr):
         ret += mid << (8 * i)
     return "0x%08X"%ret
 
+def avg_per2bytes(list):
+    ret = 0
+    list_v = [0,0,0,0]
+    for whole_value in list:
+        for i in range(4):
+            list_v[i] += (whole_value >> (8*i)) & 0xFF
+    for i in range(4):
+        list_v[i] = list_v[i] / len(list)
+        ret +=  list_v[i] << (i*8)
+    return ret
+
+
+def calc_MPWLDECTRL(ctrl_list):
+    ctrl0 = [];ctrl1 = []
+    for c in ctrl_list:
+        ctrl0.append(int(c[0],16))
+        ctrl1.append(int(c[1],16))
+    str_ctrl0 = "0x%08X"%avg_per2bytes(ctrl0)
+    str_ctrl1 = "0x%08X"%avg_per2bytes(ctrl1)
+    return str_ctrl0,str_ctrl1
 
 import unittest
 
@@ -149,22 +187,22 @@ class CheckInputFileTestCase(unittest.TestCase):
 class CheckReadDataTestCase(unittest.TestCase):
     def testReadCtrlData(self):
         txt = get_file_context(folder_path + '1.log')
-        ctrl0 = get_MMDC_MPWLDECTRL(0,txt)
-        ctrl1 = get_MMDC_MPWLDECTRL(1,txt)
-        self.assertEquals([ctrl0,ctrl1],['0x00000004','0x00150011'])
+        ctrl = get_MMDC_MPWLDECTRL(txt)
+
+        self.assertEquals(ctrl,('0x00000004','0x00150011'))
         
         byte = get_Byte(txt)
-        self.assertEquals(byte[0]['start'],['0x02','0x10'])
-        self.assertEquals(byte[0]['end'],['0x04','0x38'])
+        self.assertEquals(byte[0][0],hc_abs_value('0x02','0x10')) #start
+        self.assertEquals(byte[0][1],hc_abs_value('0x04','0x38')) #end
 
-        self.assertEquals(byte[1]['start'],['0x02','0x08'])
-        self.assertEquals(byte[1]['end'],['0x04','0x2C'])
+        self.assertEquals(byte[1][0],hc_abs_value('0x02','0x08'))
+        self.assertEquals(byte[1][1],hc_abs_value('0x04','0x2C'))
 
-        self.assertEquals(byte[2]['start'],['0x02','0x08'])
-        self.assertEquals(byte[2]['end'],['0x04','0x2C'])
+        self.assertEquals(byte[2][0],hc_abs_value('0x02','0x08'))
+        self.assertEquals(byte[2][1],hc_abs_value('0x04','0x2C'))
 
-        self.assertEquals(byte[3]['start'],['0x01','0x68'])
-        self.assertEquals(byte[3]['end'],['0x04','0x34'])
+        self.assertEquals(byte[3][0],hc_abs_value('0x01','0x68'))
+        self.assertEquals(byte[3][1],hc_abs_value('0x04','0x34'))
 
         read_cat = get_ReadCal(txt)
         for i in range(0,3):
@@ -201,10 +239,10 @@ class CalcSingleResult(unittest.TestCase):
 
     def testCalcByte(self):
         byte = get_Byte(self.txt)
-        self.assertEquals(hc_abs_value('0x03','0x38'),calc_hc_abs(byte[0]))
-        self.assertEquals(hc_abs_value('0x03','0x2C'),calc_hc_abs(byte[1]))
-        self.assertEquals(hc_abs_value('0x03','0x2C'),calc_hc_abs(byte[2]))
-        self.assertEquals(hc_abs_value('0x03','0x34'),calc_hc_abs(byte[3]))
+        self.assertEquals(hc_abs_value('0x03','0x38'),calc_hc_abs_final(byte[0]))
+        self.assertEquals(hc_abs_value('0x03','0x2C'),calc_hc_abs_final(byte[1]))
+        self.assertEquals(hc_abs_value('0x03','0x2C'),calc_hc_abs_final(byte[2]))
+        self.assertEquals(hc_abs_value('0x03','0x34'),calc_hc_abs_final(byte[3]))
 
     def testQDSResult(self):
         byte = get_Byte(self.txt)
@@ -229,6 +267,25 @@ class CalcSingleResult(unittest.TestCase):
 
         write_cat = get_WriteCal(self.txt)
         self.assertEqual('0x3C3A4240',calc_cal_value(write_cat))
+
+
+class Calc2SampleResult(unittest.TestCase):
+    def setUp(self):
+        self.data_txt_1 = get_file_context(folder_path + '1.log')
+        self.data_txt_2 = get_file_context(folder_path + '2.log')
+
+    def testMPWLDE(self):
+        file1_ctrl = get_MMDC_MPWLDECTRL(self.data_txt_1)
+        file2_ctrl = get_MMDC_MPWLDECTRL(self.data_txt_2)
+        ctrl_list = [file1_ctrl,file2_ctrl]
+        self.assertEquals(('0x00020007','0x00170010'),calc_MPWLDECTRL(ctrl_list))
+
+    def testCalcByte(self):
+        file1_byte = get_Byte(self.data_txt_1)
+        file2_byte = get_Byte(self.data_txt_2)
+        bytes = [file1_byte,file2_byte]
+        self.assertEqual(('0x032C0338','0x0330032C'),calc_DQS_cals(bytes))
+
 try:
     unittest.main()
 except:
