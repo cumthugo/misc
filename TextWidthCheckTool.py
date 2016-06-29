2583ba28d95f51ec652e80196cac3eb9e3e68d49
Zhang Yong <zhangyong_yz@hotmail.com>
2016-06-29 23:08:17 +0800

    refact sth.

--- TextWidthCheckTool.py
+++ TextWidthCheckTool.py
@@ -9,13 +9,22 @@ import sys,os
 import Fontlib
 
 #parameter config
+
+#config text id and width xls file.
 text_id_and_width_workbook = xlrd.open_workbook(r'./1.xls')
 text_id_and_width_table = text_id_and_width_workbook.sheet_by_name(u'1')
 text_id_colume_index = 2 #first colume A is 0, 2 means colume C
 max_width_colume_index = 4
 text_font_colume_index = 7
 
-xml_file_path = r'./MIBG_GP_2217_M5_20160614'
+#config xml file 
+xml_file_path = './MIBG_GP_2217_M5_20160614'
+xml_languages_file =['/Englisch_28USA29.xml','/Chinese_CN.xml', '/Portugiesisch_28Brasilien29.xml', '/Russisch.xml', '/Spanisch_28Mexiko29.xml'] 
+
+#config font file name
+font_light_file = './unittest/VWThesis_MQB_Light_140425.ttf'
+font_regular_file = './unittest/VWThesis_MQB_Regular_140425.TTF'
+font_chinese_file = './unittest/FZHT_GB18030.TTF'
 
 textInfo = {}
 for i in range(1,text_id_and_width_table.nrows):
@@ -32,12 +41,12 @@ for i in range(1,text_id_and_width_table.nrows):
 def checkwidth(lang_table):
     for k in textInfo:
         if k in lang_table:
-            eng_font_name = './unittest/VWThesis_MQB_Regular_140425.TTF'
+            eng_font_name = font_regular_file
             if textInfo[TextID]['font'] == u'VWThesis MIB Light':
-                eng_font_name = './unittest/VWThesis_MQB_Light_140425.ttf'
+                eng_font_name = font_light_file 
             check_texts = lang_table[k].split('\\n')
             for t in check_texts:
-                actual_width = Fontlib.GetTextWidth(eng_font_name,'./unittest/FZHT_GB18030.TTF',textInfo[k]['size'],t)
+                actual_width = Fontlib.GetTextWidth(eng_font_name,font_chinese_file,textInfo[k]['size'],t)
                 if actual_width > textInfo[k]['maxPixel']:
                     print k
                     print 'actual_width =',actual_width,' maxPixel =',textInfo[k]['maxPixel'],' Text =',t
