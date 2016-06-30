c0abcfb905252e8d478d5da391e19bc7faa8e89e
ZhangYong <yzhang66@yfve.com.cn>
2016-06-30 09:50:16 +0800

    refact all, fix an issue for english font selection

--- TextWidthCheckTool.py
+++ TextWidthCheckTool.py
@@ -17,39 +17,44 @@ text_id_colume_index = 2 #first colume A is 0, 2 means colume C
 max_width_colume_index = 4
 text_font_colume_index = 7
 
-#config xml file 
-xml_file_path = './MIBG_GP_2217_M5_20160614'
-xml_languages_file =['/Englisch_28USA29.xml','/Chinese_CN.xml', '/Portugiesisch_28Brasilien29.xml', '/Russisch.xml', '/Spanisch_28Mexiko29.xml'] 
+#config xml file
+xml_file_path = r'./MIBG_GP_2217_M5_20160614/'
+xml_languages_file =[r'Englisch_28USA29.xml',r'Chinese_CN.xml', r'Portugiesisch_28Brasilien29.xml', r'Russisch.xml', r'Spanisch_28Mexiko29.xml']
 
 #config font file name
-font_light_file = './unittest/VWThesis_MQB_Light_140425.ttf'
-font_regular_file = './unittest/VWThesis_MQB_Regular_140425.TTF'
-font_chinese_file = './unittest/FZHT_GB18030.TTF'
+font_light_file = r'./VWThesis_MQB_Light_140425.ttf'
+font_regular_file = r'./VWThesis_MQB_Regular_140425.TTF'
+font_chinese_file = r'./FZHT_GB18030.TTF'
 
-textInfo = {}
-for i in range(1,text_id_and_width_table.nrows):
-    TextID = text_id_and_width_table.cell(i,text_id_colume_index).value.replace('.','_')
-    if TextID[0:4] == 'LANG':
-        textInfo[TextID] = {}
-        #print TextID
-        textInfo[TextID]['font'] = text_id_and_width_table.cell(i,text_font_colume_index).value[:-4]
-        textInfo[TextID]['size'] = int(text_id_and_width_table.cell(i,text_font_colume_index).value[-2:])
-        textInfo[TextID]['maxPixel'] = int(text_id_and_width_table.cell(i,max_width_colume_index).value)
+##############################################################################
+#implementation below, please don't modify anything if you don't know, thanks.
+##############################################################################
+def read_text_id_and_max_width():
+    text_info = {}
+    for i in range(1,text_id_and_width_table.nrows):
+        TextID = text_id_and_width_table.cell(i,text_id_colume_index).value.replace('.','_')
+        if TextID[0:4] == r'LANG': #this is a filter, ignore some invalid text id
+            text_info[TextID] = {}
+            #print TextID
+            text_info[TextID]['font'] = text_id_and_width_table.cell(i,text_font_colume_index).value[:-4] #:-4 means don't get the last 4 chars
+            text_info[TextID]['size'] = int(text_id_and_width_table.cell(i,text_font_colume_index).value[-2:]) #-2: mean get the last 2 chars
+            text_info[TextID]['maxPixel'] = int(text_id_and_width_table.cell(i,max_width_colume_index).value)
+    return text_info
 
 
-
-def checkwidth(lang_table):
-    for k in textInfo:
-        if k in lang_table:
+def check_width_which_larger_than_maxwidth(lang_table):
+    text_id_width_info = read_text_id_and_max_width()
+    for text_id in text_id_width_info:
+        if text_id in lang_table:
             eng_font_name = font_regular_file
-            if textInfo[TextID]['font'] == u'VWThesis MIB Light':
-                eng_font_name = font_light_file 
-            check_texts = lang_table[k].split('\\n')
-            for t in check_texts:
-                actual_width = Fontlib.GetTextWidth(eng_font_name,font_chinese_file,textInfo[k]['size'],t)
-                if actual_width > textInfo[k]['maxPixel']:
-                    print k
-                    print 'actual_width =',actual_width,' maxPixel =',textInfo[k]['maxPixel'],' Text =',t
+            if text_id_width_info[text_id]['font'] == u'VWThesis MIB Light':
+                eng_font_name = font_light_file
+            check_texts = lang_table[text_id].split('\\n')
+            for text_line in check_texts:
+                actual_width = Fontlib.GetTextWidth(eng_font_name,font_chinese_file,text_id_width_info[text_id]['size'],text_line)
+                if actual_width > text_id_width_info[text_id]['maxPixel']:
+                    print text_id
+                    print 'actual_width =',actual_width,' maxPixel =',text_id_width_info[text_id]['maxPixel'],' Text =',text_line
                     break
 
 class readTextTableHandler(ContentHandler):
@@ -59,31 +64,11 @@ class readTextTableHandler(ContentHandler):
         if name == 'LangID':
             self.arr[attrs['ID']] = attrs['Text']
 
-English_text_table = {}
-Chinese_text_table = {}
-Brasilien_text_table = {}
-Russisch_text_table = {}
-Mexiko_text_table = {}
+def check_xml_text_width(xml_file):
+    text_table = {}
+    parse(xml_file_path + xml_file, readTextTableHandler(text_table))
+    print '\n\n\nChecking ',xml_file
+    check_width_which_larger_than_maxwidth(text_table)
 
-parse(xml_file_path + r'/Englisch_28USA29.xml',readTextTableHandler(English_text_table))
-parse(xml_file_path + r'/Chinese_CN.xml',readTextTableHandler(Chinese_text_table))
-parse(xml_file_path + r'/Portugiesisch_28Brasilien29.xml',readTextTableHandler(Brasilien_text_table))
-parse(xml_file_path + r'/Russisch.xml',readTextTableHandler(Russisch_text_table))
-parse(xml_file_path + r'/Spanisch_28Mexiko29.xml',readTextTableHandler(Mexiko_text_table))
-
-
-print 'Checking English ...'
-checkwidth(English_text_table)
-
-print '\n\n\nChecking Chinese ...'
-checkwidth(Chinese_text_table)
-
-print '\n\n\nChecking Brasilien ...'
-checkwidth(Brasilien_text_table)
-
-print '\n\n\nChecking Russisch ...'
-checkwidth(Russisch_text_table)
-
-print '\n\n\nChecking Mexiko ...'
-checkwidth(Mexiko_text_table)
-
+for xml_filename in xml_languages_file:
+    check_xml_text_width(xml_filename)
